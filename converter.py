3a579480690229fd1b10d2b9b52eca42b5a3aa9a
Walther Lalk <walther@octoplus.co.za>
2014-03-12 11:42:09 +0200

    Update for all items

--- converter.py
+++ converter.py
@@ -32,32 +32,6 @@ def delete_file(file):
 def erase_temp_files():
     delete_file(TEXTILE_DOCUMENT_FILEPATH)
     delete_file(MARKDOWN_DOCUMENT_FILEPATH)
-
-# Convert all kb articles to markdown
-for article in KbArticles.objects.all():
-    
-    # write article summary in textile in temp file
-    touch_file(article.summary)
-    # run pandoc to convert to markdown
-    subprocess.call([PANDOC_COMMAND, TEXTILE_DOCUMENT_FILEPATH, "-o", MARKDOWN_DOCUMENT_FILEPATH])
-    # read converted result from markdown file
-    new_summary = read_file()
-
-    # write article content in textile in temp file
-    touch_file(article.content)
-    # run pandoc to convert to markdown
-    subprocess.call([PANDOC_COMMAND, TEXTILE_DOCUMENT_FILEPATH, "-o", MARKDOWN_DOCUMENT_FILEPATH])
-    # read converted result
-    new_content = read_file()
-
-    # update database
-    article.content = new_content
-    article.summary = new_summary
-    article.save()
-    print "updated article ", article.id
-    
-# erase temp files
-# erase_temp_files()
     
 # Convert all wiki pages to markdown
 for wikipage in WikiContents.objects.all():
@@ -74,5 +48,82 @@ for wikipage in WikiContents.objects.all():
     wikipage.save()
     print "updated wikipage ", wikipage.id
 
-# erase temp files
+# Convert all wiki page history to markdown
+for wikipage in WikiContentVersions.objects.all():
+    
+    # write wikipage text in textile in temp file
+    touch_file(wikipage.data)
+    # run pandoc to convert to markdown
+    subprocess.call([PANDOC_COMMAND, TEXTILE_DOCUMENT_FILEPATH, "-o", MARKDOWN_DOCUMENT_FILEPATH])
+    # read converted result from markdown file
+    new_text = read_file()
+    
+    # update database
+    wikipage.data = new_text
+    wikipage.save()
+    print "updated wikipage version ", wikipage.id
+
+# Convert all issues to markdown
+for issue in Issues.objects.all():
+    
+    # write wikipage text in textile in temp file
+    touch_file(issue.description)
+    # run pandoc to convert to markdown
+    subprocess.call([PANDOC_COMMAND, TEXTILE_DOCUMENT_FILEPATH, "-o", MARKDOWN_DOCUMENT_FILEPATH])
+    # read converted result from markdown file
+    new_text = read_file()
+    
+    # update database
+    issue.description = new_text
+    issue.save()
+    print "updated issue ", issue.id
+
+# Convert all issue templates to markdown
+for issuetemplate in IssueTemplates.objects.all():
+    
+    # write wikipage text in textile in temp file
+    touch_file(issuetemplate.description)
+    # run pandoc to convert to markdown
+    subprocess.call([PANDOC_COMMAND, TEXTILE_DOCUMENT_FILEPATH, "-o", MARKDOWN_DOCUMENT_FILEPATH])
+    # read converted result from markdown file
+    new_text = read_file()
+    
+    # update database
+    issuetemplate.description = new_text
+    issuetemplate.save()
+    print "updated issue template ", issuetemplate.id
+
+# Convert all issue journals to markdown
+for issuejournal in IssueJournals.objects.all():
+    
+    if (issuejournal.notes is None) or (issuejournal.notes == ""):
+        continue
+
+    # write wikipage text in textile in temp file
+    touch_file(issuejournal.notes)
+    # run pandoc to convert to markdown
+    subprocess.call([PANDOC_COMMAND, TEXTILE_DOCUMENT_FILEPATH, "-o", MARKDOWN_DOCUMENT_FILEPATH])
+    # read converted result from markdown file
+    new_text = read_file()
+    
+    # update database
+    issuejournal.notes = new_text
+    issuejournal.save()
+    print "updated issue journal ", issuejournal.id
+
+# Convert all projects to markdown
+for project in Projects.objects.all():
+    
+    # write wikipage text in textile in temp file
+    touch_file(project.description)
+    # run pandoc to convert to markdown
+    subprocess.call([PANDOC_COMMAND, TEXTILE_DOCUMENT_FILEPATH, "-o", MARKDOWN_DOCUMENT_FILEPATH])
+    # read converted result from markdown file
+    new_text = read_file()
+    
+    # update database
+    project.description = new_text
+    project.save()
+    print "updated project ", project.id
+
 erase_temp_files()
